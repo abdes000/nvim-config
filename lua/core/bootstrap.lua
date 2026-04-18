98d5926084148c4a136d777d6f8be307b5c48c50
abdes000 <rohan2007tulipss@gmail.com>
2026-04-18 10:46:04 +0530

    nvim-config

--- lua/core/bootstrap.lua
+++ lua/core/bootstrap.lua
@@ -1,19 +1,18 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
+
+-- require("lazy").setup("plugins")
 
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
@@ -36,6 +35,8 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     -- { import = "core" },
-    { import = "plugins" } -- this will recursively load lua/plugins/**/*
+    { import = "core" },
+    { import = "plugins" }, -- this will recursively load lua/plugins/**/*
   },
 })
+require("lazy").setup("plugins")
